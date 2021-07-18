cfc1f87c3666c2ee76222de46bc4c3716bef4229
Nors1e <hisurfingc@yahoo.com>
2021-07-17 22:22:49 -0700

    added @params to price sythesizer

---
+++ aws_price_synth.py
@@ -0,0 +1,5 @@
+#SYNTHESIZER 
+
+#Iterate through the  dict
+#get @params(price, cpu, OS) and store them in a dict
+#load params into a new json file
--- aws_pricing.py
+++ aws_pricing.py
@@ -1,5 +1,6 @@
 import json
 import boto3
+import objectpath
 import pprint
  
 pricing = boto3.client('pricing', region_name='us-east-1')
@@ -15,13 +16,22 @@ response = pricing.get_products(
          {'Type' :'TERM_MATCH', 'Field':'memory',          'Value':'256 GiB'              },
          {'Type' :'TERM_MATCH', 'Field':'location',        'Value':'US East (N. Virginia)'}
      ],
-     MaxResults=20
+     MaxResults=1
 )
- 
-# displays all ec2 info organized
-# for price in response['PriceList']:
-#  pp = pprint.PrettyPrinter(indent=1, width=300)
-#  pp.pprint(json.loads(price))
-# print()
-with open('data.json', 'w') as f:
- f.write(json.dumps(response.get(pricing, f)))
+
+# print(response['PriceList'])
+
+#displays all ec2 info organized
+for price in response['PriceList']:
+ pp = pprint.PrettyPrinter(indent=1, width=300)
+ pp.pprint(json.loads(price))
+print()
+# with open('data.json', 'w') as f:
+#  f.write(json.dumps(response.get(pricing, f)))
+
+# with open('data.json', 'w') as f:
+#  json.dumps(pricing.get(response['PriceList'], f))
+
+# with open('data.json', 'w') as fp:
+#     foo = json.dumps(response['PriceList'])
+#     print(foo)

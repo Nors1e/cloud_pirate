e38b5bc409387ae0f6e30505b7c61e5cb1db485b
Nors1e <hisurfingc911@gmail.com>
2021-09-01 23:20:21 -0700

    server for user  identification data added

--- aws_noinput.py
+++ aws_noinput.py
@@ -1,9 +1,8 @@
 #TODO: Convert this to a def page to call to main
-
 import json
 import boto3
 
-
+#TODO: Add a way for the program to detect where the user is and base regionality on that 
 #desired_region = input("Input your desired region: ")
 pricing = boto3.client('pricing', region_name='us-east-1')
 
@@ -21,13 +20,22 @@ response = pricing.get_products(
     MaxResults=20
 )
 
+
 product_attributes = {}
 
 # stores info in a list due to overwriting and converts data into json
 for entry_string in response["PriceList"]:
     entry = json.loads(entry_string)
+#TODO: add a way to include the product name, allow the user to query product information associated with that product
+    sku_num = entry.get('product').get('sku')
+    unit_num = sku_num + '.' + 'JRTCKXETXF'
+    unit_num_one = sku_num + '.' + 'JRTCKXETXF' + '.' + '6YS6EN2CT7'
+
     on_demand = entry["terms"]["OnDemand"].popitem()[1]
+    x = on_demand.get("priceDimensions").get(unit_num_one)['description']
+    print(x)
     product_attributes[on_demand["sku"]] = on_demand["priceDimensions"].popitem()[1]["pricePerUnit"]
+
 
 #servers sorted in ascending order based on price
 sort_servers = dict(sorted(product_attributes.items(), key=lambda item: float(item[1]["USD"])))
@@ -44,3 +52,5 @@ pairs_iterator = iter(dict_pairs)
 first_pair = next(pairs_iterator)
 
 print(first_pair)
+
+#TODO: Create a way for users to iterate through and print out a set index amount
